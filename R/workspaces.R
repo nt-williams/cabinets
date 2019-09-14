b0a503ea4fed57e91a0ab2ca3974bc98e255ef98
Nick Williams <ntwilliams.personal@gmail.com>
2019-09-14 17:41:37 -0400

    list() instead of match.arg()

--- R/workspaces.R
+++ R/workspaces.R
@@ -161,7 +161,7 @@ new_cabinet_proj <- function(cabinet,
     purrr::walk(proj_folders, ~dir.create(.x, recursive = TRUE))
 
     if (r_project) {
-        proj_settings <- create_r_proj(match.arg(...))
+        proj_settings <- create_r_proj(list(...))
         r_project <- file.path(proj_path, paste0(project_name, ".Rproj"))
         cat(proj_settings, file = r_project)
     } else {
@@ -172,7 +172,7 @@ new_cabinet_proj <- function(cabinet,
         cat(glue::glue("Opening new R project, {project_name}"))
         cat("\nR project settings:\n")
         cat("\n")
-        create_r_proj(match.arg(...))
+        create_r_proj(list(...))
         Sys.sleep(2)
         if (usethis::proj_activate(proj_path)) {
             on.exit()
