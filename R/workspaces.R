599641a6447c267b5ef6b39a6967fab5a7e9bf36
Nick Williams <ntwilliams.personal@gmail.com>
2019-09-14 15:07:51 -0400

    create_r_project

--- R/workspaces.R
+++ R/workspaces.R
@@ -79,10 +79,44 @@ get_cabinets <- function() {
 
 }
 
+create_r_proj <- function(Version = 1.0,
+                          RestoreWorkspace = c("No", "Yes", "Default"),
+                          SaveWorkspace = c("No", "Yes", "Default"),
+                          AlwaysSaveHistory = c("Default", "No", "Yes"),
+                          EnableCodeIndexing = c("Yes", "No", "Default"),
+                          UseSpacesForTab = c("Yes", "No", "Default"),
+                          NumSpacesForTab = 2,
+                          Encoding = "UTF-8",
+                          RnwWeave = "Sweave",
+                          LaTeX = "pdfLaTeX",
+                          AutoAppendNewLine = "Yes",
+                          StripTrailingWhiteSpace = "Yes") {
+
+    restore <- match.arg(RestoreWorkspace)
+    save <- match.arg(SaveWorkspace)
+    always <- match.arg(AlwaysSaveHistory)
+    code_index <- match.arg(EnableCodeIndexing)
+    space_tabs <- match.arg(UseSpacesForTab)
+
+    glue::glue(
+    'Version: {Version}
+    RestoreWorkspace: {restore}
+    SaveWorkspace: {save}
+    AlwaysSaveHistory: {always}
+    EnableCodeIndexing: {code_index}
+    UseSpacesForTab: {space_tabs}
+    NumSpacesForTab: {NumSpacesForTab}
+    Encoding: {Encoding}
+    RnwWeave: {RnwWeave}
+    LaTeX: {LaTeX}
+    AutoAppendNewline: {AutoAppendNewLine}
+    StripTrailingWhitespace: {StripTrailingWhiteSpace}')
+}
+
 new_cabinet_proj <- function(cabinet,
                              project_name,
                              r_project = TRUE,
-                             open = TRUE) {
+                             open = TRUE, ...) {
 
     # check_cabinet()
 
@@ -95,8 +129,9 @@ new_cabinet_proj <- function(cabinet,
     purrr::walk(proj_folders, ~dir.create(.x, recursive = TRUE))
 
     if (r_project) {
+        proj_settings <- create_r_proj()
         r_project <- file.path(proj_path, paste0(project_name, ".Rproj"))
-        file.create(r_project, recursive = TRUE)
+        cat(proj_settings, file = r_project)
     } else {
         open <- FALSE
     }
