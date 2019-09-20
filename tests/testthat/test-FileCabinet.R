a704abf418002cde798bb9baea108f1830424e49
Nick Williams <ntwilliams.personal@gmail.com>
2019-09-20 12:06:54 -0400

    all important checks have been tested

---
+++ tests/testthat/test-FileCabinet.R
@@ -0,0 +1,9 @@
+context("reate FileCabinet")
+
+test_that("creating FileCabinet", {
+    x <- FileCabinet$new(name = "test_cab",
+                         directory = "a/random/path",
+                         structure = list('test' = NULL))
+
+    expect_output(x$print())
+})
