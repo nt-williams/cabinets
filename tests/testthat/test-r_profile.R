dc43251c2f52981a3e6967330e1eec9647912372
Nick Williams <ntwilliams.personal@gmail.com>
2019-09-20 11:59:45 -0400

    80% coverage in checks

--- tests/testthat/test-r_profile.R
+++ tests/testthat/test-r_profile.R
@@ -1,12 +1,16 @@
 context("check_r_profile")
 
 test_that("doing nothing if .Rprofile already exists", {
-  og <- getwd()
-  setwd(tempdir())
-  withr::with_file(
-      file.create(file.path(tempdir(), ".Rprofile")), {
-          expect_output(check_r_profile(), "OK")
-          setwd(og)
-      }
-  )
+  withr::with_dir(tempdir(), {
+      file.create(file.path(tempdir(), ".Rprofile"))
+      expect_output(check_r_profile(), "OK")
+      unlink(file.path(tempdir(), ".Rprofile"))
+  })
 })
+
+test_that("writing .Rprofile", {
+  withr::with_dir(tempdir(), {
+      expect_output(check_r_profile())
+      unlink(file.path(tempdir(), ".Rprofile"))
+    })
+  })
